8641e565f4ba7dd9b5946dd5fc826469b39cb5f0
kerimk2 <kkassem17@gmail.com>
2025-07-27 01:48:28 +0400

    Update server.js
    Updating server to include pinecone project ID

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ const openai = new OpenAI({
 const pinecone = new Pinecone({
   apiKey: process.env.PINECONE_API_KEY,
   environment: process.env.PINECONE_ENVIRONMENT,
+  projectId: process.env.PINECONE_PROJECT_ID,
 });
 
 let index;
